024a9012821efbd77337f98fa7b61de29de10a59
Hibariya <hibariya@gmail.com>
2020-10-05 19:39:49 +0900

    Move files into ci/; use .env file to share variables between runner and web

--- spike.sh
+++ spike.sh
@@ -1,7 +1,4 @@
-#!/bin/bash -e
-
-sudo curl -o /usr/bin/jq -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
-sudo chmod +x /usr/bin/jq
+#!/bin/bash -xe
 
 ## stripe-samples/subscription-use-cases
 
@@ -10,9 +7,8 @@ git clone https://github.com/hibariya/${REPOSITORY_NAME}.git
 cd "$REPOSITORY_NAME"
 git checkout ci
 
-cp -p ../run_tests .
-cp ../docker/test.yml docker-compose.yml
-cp ../docker/test/Dockerfile .
+cp -pr ../ci .
+export COMPOSE_FILE=ci/docker/docker-compose.base.yml
 docker-compose build runner
 
 
@@ -22,31 +18,27 @@ export SERVER_JAR_FILE=target/subscriptions-with-fixed-price-1.0.0-SNAPSHOT-jar-
 export TEST_FILES=spec/fixed_price_server_spec.rb
 for lang in $(cat .cli.json | jq -r ".integrations[] | select(.name==\"${SAMPLE}\") | .servers | .[]")
 do
-  touch ${SAMPLE}/server/${lang}/.env # FIXME: use .env file to load environment variables?
+  echo > ${SAMPLE}/server/${lang}/.env
   export SERVER_LANG=$lang
+  export COMPOSE_FILE=ci/docker/docker-compose.base.yml:ci/docker/${lang}/docker-compose.web.yml
 
-  # TODO: want to move into a directory that won't conflict with existing files
-  cp ../docker/${lang}.yml docker-compose.override.yml
-  cp -pr ../docker/${lang} .
-
-  ./run_tests "$TEST_FILES"
+  ./ci/run_tests "$TEST_FILES"
 done
 
 
 export SAMPLE=per-seat-subscriptions
 export STATIC_DIR=../../client
 export SERVER_JAR_FILE=target/subscriptions-with-per-seat-pricing-1.0.0-SNAPSHOT-jar-with-dependencies.jar
-export TEST_FILES=spec/per_seat_server_spec.rb
+export TEST_FILES=spec/per_seat_server_dummy_spec.rb # spec/per_seat_server_spec.rb
 for lang in $(cat .cli.json | jq -r ".integrations[] | select(.name==\"${SAMPLE}\") | .servers | .[]")
 do
-  touch ${SAMPLE}/server/${lang}/.env
+  echo > ${SAMPLE}/server/${lang}/.env
   export SERVER_LANG=$lang
+  export COMPOSE_FILE=ci/docker/docker-compose.base.yml:ci/docker/${lang}/docker-compose.web.yml
 
-  cp ../docker/${lang}.yml docker-compose.override.yml
-  cp -pr ../docker/${lang} .
-
-  ./run_tests "$TEST_FILES"
+  ./ci/run_tests "$TEST_FILES"
 done
+
 
 export SAMPLE=usage-based-subscriptions
 export STATIC_DIR=../../client
@@ -55,11 +47,9 @@ export TEST_FILES=spec/usage_based_server_spec.rb
 rm -rf ${SAMPLE}/server/dotnet/ReportUsage # causes "Program.cs(14,28): error CS0017: Program has more than one entry point defined."
 for lang in $(cat .cli.json | jq -r ".integrations[] | select(.name==\"${SAMPLE}\") | .servers | .[]")
 do
-  touch ${SAMPLE}/server/${lang}/.env
+  echo > ${SAMPLE}/server/${lang}/.env
   export SERVER_LANG=$lang
+  export COMPOSE_FILE=ci/docker/docker-compose.base.yml:ci/docker/${lang}/docker-compose.web.yml
 
-  cp ../docker/${lang}.yml docker-compose.override.yml
-  cp -pr ../docker/${lang} .
-
-  ./run_tests "$TEST_FILES"
+  ./ci/run_tests "$TEST_FILES"
 done
