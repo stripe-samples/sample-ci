115ddd94ef58abd19d71e68c49686ed2ddd905c4
Hibariya <hibariya@gmail.com>
2020-10-10 08:36:22 +0900

    Create docker-compose{,.override}.yml files for each test to make debugging easier

--- fail_per_seat_python.sh
+++ fail_per_seat_python.sh
@@ -1,23 +1,18 @@
 #!/bin/bash -xe
 
 export REPOSITORY_NAME=subscription-use-cases
-git clone https://github.com/hibariya/${REPOSITORY_NAME}.git
-cd "$REPOSITORY_NAME"
+git clone https://github.com/hibariya/subscription-use-cases.git
+cd subscription-use-cases
 git checkout ci
 
 cp -pr ../ci .
-export COMPOSE_FILE=ci/docker/docker-compose.base.yml
-docker-compose build runner
+cp -p ci/docker/docker-compose.base.yml docker-compose.yml
+docker-compose build runner # runner runs RSpec tests for each sample
 
-
+sample=per-seat-subscriptions
 lang=python
-export SAMPLE=per-seat-subscriptions
-export STATIC_DIR=../../client
-export TEST_FILES=spec/per_seat_server_spec.rb
-export SERVER_LANG=$lang
-export COMPOSE_FILE=ci/docker/docker-compose.base.yml:ci/docker/${lang}/docker-compose.web.yml
+ci/configure_docker_compose "$sample" "$lang" ../../client \
+                            target/subscriptions-with-per-seat-pricing-1.0.0-SNAPSHOT-jar-with-dependencies.jar
 
-echo > ${SAMPLE}/server/${lang}/.env
-./ci/run_tests "$TEST_FILES"
-
-docker-compose stop
+echo > ${sample}/server/${lang}/.env
+ci/run_tests spec/per_seat_server_spec.rb
--- spike.sh
+++ spike.sh
@@ -1,59 +1,49 @@
 #!/bin/bash -xe
 
 # Clone a sample repository and copy sample-ci files into it
-export REPOSITORY_NAME=subscription-use-cases
-git clone https://github.com/hibariya/${REPOSITORY_NAME}.git
-cd "$REPOSITORY_NAME"
+git clone https://github.com/hibariya/subscription-use-cases.git
+cd subscription-use-cases
 git checkout ci
 
 cp -pr ../ci .
-export COMPOSE_FILE=ci/docker/docker-compose.base.yml
+cp -p ci/docker/docker-compose.base.yml docker-compose.yml
+
+export SAMPLE=NA; export SERVER_LANG=NA; export STATIC_DIR=NA;
 docker-compose build runner # runner runs RSpec tests for each sample
 
+server_langs_in_sample() {
+  jq -r ".integrations[] | select(.name==\"${1}\") | .servers | .[]"
+}
 
-# The exported environment variables will be read by ci/docker/.env and ci/docker/docker-compose.base.yml
-export SAMPLE=fixed-price-subscriptions
-export STATIC_DIR=../../client/vanillajs
-export SERVER_JAR_FILE=target/subscriptions-with-fixed-price-1.0.0-SNAPSHOT-jar-with-dependencies.jar
-export TEST_FILES=spec/fixed_price_server_spec.rb
-for lang in $(cat .cli.json | jq -r ".integrations[] | select(.name==\"${SAMPLE}\") | .servers | .[]")
+sample=fixed-price-subscriptions
+for lang in $(cat .cli.json | server_langs_in_sample "$sample")
 do
-  echo > ${SAMPLE}/server/${lang}/.env # Empty the file since the process on the container already has those environment variables
-  export SERVER_LANG=$lang
-  export COMPOSE_FILE=ci/docker/docker-compose.base.yml:ci/docker/${lang}/docker-compose.web.yml
+  ci/configure_docker_compose "$sample" "$lang" ../../client/vanillajs \
+                              target/subscriptions-with-fixed-price-1.0.0-SNAPSHOT-jar-with-dependencies.jar
 
-  ./ci/run_tests "$TEST_FILES" # Build containers and run $TEST_FILES
+  echo > ${sample}/server/${lang}/.env # Empty the file since the process on the container already has those environment variables
+  ci/run_tests spec/fixed_price_server_spec.rb # Build containers and run tests
 done
 
-
-# Do over again for per-seat-subscriptions
-export SAMPLE=per-seat-subscriptions
-export STATIC_DIR=../../client
-export SERVER_JAR_FILE=target/subscriptions-with-per-seat-pricing-1.0.0-SNAPSHOT-jar-with-dependencies.jar
-export TEST_FILES=spec/per_seat_server_dummy_spec.rb # spec/per_seat_server_spec.rb will fail on python, see fail_per_seat_python.sh
-for lang in $(cat .cli.json | jq -r ".integrations[] | select(.name==\"${SAMPLE}\") | .servers | .[]")
+sample=per-seat-subscriptions
+for lang in $(cat .cli.json | server_langs_in_sample "$sample")
 do
-  echo > ${SAMPLE}/server/${lang}/.env
-  export SERVER_LANG=$lang
-  export COMPOSE_FILE=ci/docker/docker-compose.base.yml:ci/docker/${lang}/docker-compose.web.yml
+  ci/configure_docker_compose "$sample" "$lang" ../../client \
+                              target/subscriptions-with-per-seat-pricing-1.0.0-SNAPSHOT-jar-with-dependencies.jar
 
-  ./ci/run_tests "$TEST_FILES"
+  echo > ${sample}/server/${lang}/.env
+  ci/run_tests spec/per_seat_server_dummy_spec.rb # spec/per_seat_server_spec.rb will fail on python, see fail_per_seat_python.sh
 done
 
-
-# For usage-based-subscriptions
-export SAMPLE=usage-based-subscriptions
-export STATIC_DIR=../../client
-export SERVER_JAR_FILE=target/subscriptions-with-metered-usage-1.0.0-SNAPSHOT-jar-with-dependencies.jar
-export TEST_FILES=spec/usage_based_server_spec.rb
-rm -rf ${SAMPLE}/server/dotnet/ReportUsage # causes "Program.cs(14,28): error CS0017: Program has more than one entry point defined."
-for lang in $(cat .cli.json | jq -r ".integrations[] | select(.name==\"${SAMPLE}\") | .servers | .[]")
+sample=usage-based-subscriptions
+rm -rf ${sample}/server/dotnet/ReportUsage # causes "Program.cs(14,28): error CS0017: Program has more than one entry point defined."
+for lang in $(cat .cli.json | server_langs_in_sample "$sample")
 do
-  echo > ${SAMPLE}/server/${lang}/.env
-  export SERVER_LANG=$lang
-  export COMPOSE_FILE=ci/docker/docker-compose.base.yml:ci/docker/${lang}/docker-compose.web.yml
+  ci/configure_docker_compose "$sample" "$lang" ../../client \
+                              target/subscriptions-with-metered-usage-1.0.0-SNAPSHOT-jar-with-dependencies.jar
 
-  ./ci/run_tests "$TEST_FILES"
+  echo > ${sample}/server/${lang}/.env
+  ci/run_tests spec/usage_based_server_spec.rb
 done
 
 docker-compose stop
