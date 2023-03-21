5e0a43baaa2cc4763da8384d9acbb35bd4ec6704
CJ Avilla <cjavilla@stripe.com>
2023-03-21 09:42:59 -0400

    Upgrade pip before installing deps

--- docker/python/entrypoint.sh
+++ docker/python/entrypoint.sh
@@ -3,6 +3,7 @@
 export FLASK_APP=server.py FLASK_ENV=${FLASK_ENV:-development}
 
 if [[ "$#" -eq "0" ]]; then
+  python -m pip install --upgrade pip
   pip install -r requirements.txt
   exec python3 -m flask run --port=4242 --host=0.0.0.0
 else
